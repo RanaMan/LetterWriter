c303d526cf06c7a524176def34a9f8893df1123d
Rana Puri <rana.puri@gmail.com>
2016-10-17 23:19:58 -0400

    Just making some more changes... all good...

--- src/Main.java
+++ src/Main.java
@@ -7,38 +7,45 @@ public class Main {
     Prints out a form letter with calculated mortgage payment amounts.
      */
     public static void main (String[] args){
-        double interestRate;
-        interestRate = 3.5;
+        double intrestRate;
+        intrestRate = 3.5;
+        int principal = 125000; //in dollars
+        int numberOfYears = 30;
+        double futureInterestRate =2.5;
+        double futureMonthlyPayment = getMonthlyPayment(principal,futureInterestRate,numberOfYears);
+        double monthlyPayment = getMonthlyPayment(principal,intrestRate,numberOfYears);
+        String name = "RanaMan";
 
 
-        System.out.println("Greetings! Hello Rana...");
+        printFormLetter(intrestRate, futureInterestRate, futureMonthlyPayment, monthlyPayment, name);
+    }
+
+    private static void printFormLetter(double intrestRate, double futureInterestRate, double futureMonthlyPayment, double monthlyPayment, String name) {
+        System.out.println("Greetings "+name+"!");
         System.out.println();
         System.out.println("If you like low, low mortgage payments, ");
         System.out.println("I have an opportunity you will LOVE.");
+        System.out.println("Interest rates have fallen to "+ intrestRate + "%.");
 
-        System.out.println("Interest rates have fallen to "+ interestRate + "%.");
 
-        int principal = 125000; //in dollars
-        int numberOfYears = 30;
-        int numberOfPayments = numberOfYears *12;
-        double monthlyRate = interestRate /12/100; // Convert from percentage.
-
-        double monthlyPayment = principal * (monthlyRate / (1 - Math.pow(1+ monthlyRate,-numberOfPayments)));
         System.out.format("You could pay as little as $%.2f a month for a 30 year mortgage.\n", monthlyPayment);
-        double totalCost = monthlyPayment * numberOfPayments;
-        System.out.format("(Total costs: $%.2f )\n\n", totalCost);
 
-        numberOfYears = 15;
-        numberOfPayments = numberOfYears *12;
-        monthlyPayment = principal * (monthlyRate / (1 - Math.pow(1+ monthlyRate,-numberOfPayments)));
-        System.out.format("You could pay as little as $%.2f a month for a 15 year mortgage.\n", monthlyPayment);
-        totalCost = monthlyPayment * numberOfPayments;
-        System.out.format("(Total costs: $%.2f )\n\n", totalCost);
 
         System.out.println();
 
-        double futureInterestRate =2.5;
         System.out.println("Could they fall to "+futureInterestRate+" %?");
-        System.out.println("Of course not! You should refinance immediately!");
+        System.out.format("Could pay as little as $%.2f a month for a 30 year mortgage instead?\n", futureMonthlyPayment);
+        System.out.println("Of course not! You should refinance immediately!\n");
     }
+
+
+    public static double getMonthlyPayment(int principal, double intrestRate, int numberOfYears){
+        int numberOfPayments = numberOfYears *12;
+        double monthlyRate = intrestRate /12/100; // Convert from percentage.
+        double monthlyPayment = principal * (monthlyRate / (1 - Math.pow(1+ monthlyRate,-numberOfPayments)));
+        return monthlyPayment;
+    }
+
+
+
 }
